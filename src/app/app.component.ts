e2f120c3618adb20cfb67df8b7f75a0f32120fc4
oraculo-nl <info@oraculo.nl>
2021-08-17 14:30:04 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+
+class Klant {
+  tel: number[] = [];
+  constructor(t: number[]) {
+    this.tel = t;
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularFundamentals-StructuurDirectives';
+  public title = 'AngularFundamentals-StructuurDirectives';
+  public namen: string[] = ['Elon', 'Jeff', 'Richard'];
+  public klant1: Klant = new Klant([8378374, 345345, 34545]);
+  public klant2: Klant = new Klant([]);
+  superhero = 'Spiderman';
 }
